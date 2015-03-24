ff6916c1a65ba4512287df9a5ed0efa3f4c9c7f2
mengji <mxrggu2013@gmail.com>
2015-03-23 22:04:07 -0700

    wrong code

---
+++ eventsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  eventsViewController.swift
+//  
+//
+//  Created by Xiangrui on 3/23/15.
+//
+//
+
+import Cocoa
+
+class eventsViewController: UITableViewController {
+
+}
--- iContacts/selectPersonViewController.swift
+++ iContacts/selectPersonViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
-
+protocol ViewControllerDataSource : class {
+    func userSelectedContact(contact:APContact)
+}
 class selectPersonViewController: UITableViewController, UISearchBarDelegate{
+    
+    var delegate:ViewControllerDataSource?
     
     @IBOutlet weak var searchBar: UISearchBar!
     let addressBook = APAddressBook()
@@ -85,6 +89,8 @@ class selectPersonViewController: UITableViewController, UISearchBarDelegate{
         cell.name.text = (firstName ?? "") + " " + (lastName ?? "")
         return cell
     }
+    
+
     /*override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("selectCell", forIndexPath: indexPath) as contact
         let seletedContact = contacts[indexPath.row] as APContact
@@ -96,7 +102,8 @@ class selectPersonViewController: UITableViewController, UISearchBarDelegate{
     }*/
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-
+        self.delegate?.userSelectedContact(contacts[indexPath.row] as APContact)
+        self.navigationController?.popViewControllerAnimated(true)
         
     }
     
